ccbad6577f058a7e2bd0c0cffcdcf14e1f488dde
cryptic20 <sherwin.wyco20@gmail.com>
2019-05-13 19:41:06 -0400

    added insert functions on tables

--- src/main/java/com/herokuapp/database/Database.java
+++ src/main/java/com/herokuapp/database/Database.java
@@ -38,8 +38,9 @@ public class Database {
   private final static String MovesTable =
       "CREATE TABLE IF NOT EXISTS AttackMoves (" + "MoveId integer PRIMARY KEY , \n"
           + "Name text NOT NULL UNIQUE, \n" + "Type text NOT NULL, \n" // move type
-          + "Damage integer NOT NULL, \n"
-          + "Accuracy integer NOT NULL)"; //1-100 value only. when attacking accuracy/100 for the chance
+          + "Damage integer NOT NULL, \n" + "Accuracy integer NOT NULL)"; // 1-100 value only. when
+                                                                          // attacking accuracy/100
+                                                                          // for the chance
 
 
   // create table PlayerPokemon which holds all the players pokemons
@@ -61,13 +62,45 @@ public class Database {
 
 
   public static void main(String[] args) {
-     createNewTable(PlayerTable);
-     createNewTable(PlayerData);
-     createNewTable(PokedexTable); //all pokemons will be stored here
-     createNewTable(MovesTable); //all moves will be stored here
-     createNewTable(PokemonMovesTable); //pokemons with their move will be stored here
-     createNewTable(PlayerPokemonTable); //player's pokemon will be stored here
-    createUser("admin", "HelloWorld123", "adminator");
+    // createNewTable(PlayerTable);
+    // createNewTable(PlayerData);
+    // createNewTable(PokedexTable); // all pokemons will be stored here
+    // createNewTable(MovesTable); // all moves will be stored here
+    // createNewTable(PokemonMovesTable); // pokemons with their move will be stored here
+    // createNewTable(PlayerPokemonTable); // player's pokemon will be stored here
+    // insertPokemon("Bulbasaur", "grass", 45);
+    // insertPokemon("Ivysaur", "grass", 60);
+    // insertPokemon("Venusaur", "grass", 80);
+    // insertPokemon("Charmander", "fire", 39);
+    // insertPokemon("Charmeleon", "fire", 58);
+    // insertPokemon("Charizard", "fire", 78);
+    // insertPokemon("Squirtle", "water", 44);
+    // insertPokemon("Wartortle", "water", 59);
+    // insertPokemon("Blastoise", "water", 79);
+    // insertPokemon("Caterpie", "bug", 45);
+    // insertPokemon("Metapod", "bug", 50);
+    // insertPokemon("Butterfree", "bug", 60);
+    // insertPokemon("Weedle", "bug", 40);
+    // insertPokemon("Kakuna", "bug", 45);
+    // insertPokemon("Beedrill", "bug", 65);
+    // insertPokemon("Pidgey", "flying", 40);
+    // insertPokemon("Pidgeotto", "flying", 63);
+    // insertPokemon("Pidgeot", "flying", 83);
+    // insertPokemon("Rattata", "normal", 30);
+    // insertPokemon("Raticate", "normal", 50);
+    // insertPokemon("Spearow", "flying", 40);
+    // insertPokemon("Fearow", "flying", 65);
+    // insertPokemon("Ekans", "poison", 35);
+    // insertPokemon("Arbok", "poison", 60);
+    // insertPokemon("Pikachu", "electric", 35);
+    // insertPokemon("Raichu", "electric", 60);
+    // insertPokemon("Sandshrew", "ground", 50);
+    // insertPokemon("SandSlash", "ground", 75);
+    // insertMove("Growl", "normal", 0, 100);
+    // insertMove("Tacke", "normal", 40, 100);
+    // insertMove("Vine Whip", "grass", 45, 100);
+
+
 
   }
 
@@ -96,12 +129,13 @@ public class Database {
 
   }
 
-  public static void insertPokemons(String pokemonName, String password, String nickname) {
+  public static void insertMove(String moveName, String type, int damage, int accuracy) {
     Connection c = null;
     Statement stmt = null;
 
-    String sql = "INSERT INTO Players (Username, Password, Nickname) VALUES ('" + username + "', '"
-        + password + "', '" + nickname + "')";
+    String sql = String.format(
+        "INSERT INTO AttackMoves (Name, Type, Damage, Accuracy) VALUES('%s', '%s', '%d', '%d')",
+        moveName, type, damage, accuracy);
     System.out.println(sql);
     try {
       c = DriverManager.getConnection(url);
@@ -111,19 +145,42 @@ public class Database {
       stmt.close();
       c.commit();
       c.close();
-      System.out.println("inserted Data");
+      System.out.println("inserted Data!");
     } catch (Exception e) {
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
       System.exit(0);
     }
   }
-  
+
+  public static void insertPokemon(String pokeName, String type, int hp) {
+    Connection c = null;
+    Statement stmt = null;
+
+    String sql = String.format("INSERT INTO Pokedex (Name, Type, BaseHP) VALUES('%s', '%s', '%d')",
+        pokeName, type, hp);
+    System.out.println(sql);
+    try {
+      c = DriverManager.getConnection(url);
+      c.setAutoCommit(false);
+      stmt = c.createStatement();
+      stmt.executeUpdate(sql);
+      stmt.close();
+      c.commit();
+      c.close();
+      System.out.println("inserted Data!");
+    } catch (Exception e) {
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    }
+  }
+
   public static void createUser(String username, String password, String nickname) {
     Connection c = null;
     Statement stmt = null;
+    String sql = String.format(
+        "INSERT INTO PLayers (Username, Password, Nickname) VALUES ('%s', '%s', '%s')", username,
+        password, nickname);
 
-    String sql = "INSERT INTO Players (Username, Password, Nickname) VALUES ('" + username + "', '"
-        + password + "', '" + nickname + "')";
     System.out.println(sql);
     try {
       c = DriverManager.getConnection(url);
@@ -140,10 +197,11 @@ public class Database {
     }
   }
 
-/**
- * This will create a new table on the database Pokemon_db
- * @param sql SQL query to be used for the creation of database.
- */
+  /**
+   * This will create a new table on the database Pokemon_db
+   * 
+   * @param sql SQL query to be used for the creation of database.
+   */
   public static void createNewTable(String sql) {
 
     try (Connection conn = DriverManager.getConnection(url);
