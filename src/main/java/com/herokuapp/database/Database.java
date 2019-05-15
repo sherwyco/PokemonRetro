87ac05436b48334b48aa4377d0ce2be23d491553
cryptic20 <sherwin.wyco20@gmail.com>
2019-05-14 20:09:36 -0400

    added user

--- src/main/java/com/herokuapp/database/Database.java
+++ src/main/java/com/herokuapp/database/Database.java
@@ -60,231 +60,232 @@ public class Database {
 
 
   public static void main(String[] args) {
-    createNewTable(PlayerTable);
-    createNewTable(PlayerData);
-    createNewTable(PokedexTable); // all pokemons will be stored here
-    createNewTable(MovesTable); // all moves will be stored here
-    createNewTable(PokemonMovesTable); // pokemons with their move will be stored here
-    createNewTable(PlayerPokemonTable); // player's pokemon will be stored here
-    // name, type, atk, def, hp
-    insertPokemon("Bulbasaur", "grass", 49, 49, 45);
-    insertPokemon("Ivysaur", "grass", 62, 63, 60);
-    insertPokemon("Venusaur", "grass", 82, 83, 80);
-    insertPokemon("Charmander", "fire", 52, 43, 39);
-    insertPokemon("Charmeleon", "fire", 64, 58, 58);
-    insertPokemon("Charizard", "fire", 84, 78, 78);
-    insertPokemon("Squirtle", "water", 48, 65, 44);
-    insertPokemon("Wartortle", "water", 63, 80, 59);
-    insertPokemon("Blastoise", "water", 83, 100, 79);
-    insertPokemon("Caterpie", "bug", 30, 35, 45);
-    insertPokemon("Metapod", "bug", 20, 55, 50);
-    insertPokemon("Butterfree", "bug", 45, 50, 60);
-    insertPokemon("Weedle", "bug", 35, 30, 40);
-    insertPokemon("Kakuna", "bug", 25, 50, 45);
-    insertPokemon("Beedrill", "bug", 90, 40, 65);
-    insertPokemon("Pidgey", "flying", 45, 40, 40);
-    insertPokemon("Pidgeotto", "flying", 60, 55, 63);
-    insertPokemon("Pidgeot", "flying", 80, 75, 83);
-    insertPokemon("Rattata", "normal", 56, 35, 30);
-    insertPokemon("Raticate", "normal", 81, 60, 50);
-    insertPokemon("Spearow", "flying", 60, 30, 40);
-    insertPokemon("Fearow", "flying", 90, 65, 65);
-    insertPokemon("Ekans", "poison", 60, 44, 35);
-    insertPokemon("Arbok", "poison", 85, 69, 60);
-    insertPokemon("Pikachu", "electric", 55, 40, 35);
-    insertPokemon("Raichu", "electric", 90, 55, 60);
-    insertPokemon("Sandshrew", "ground", 75, 85, 50);
-    insertPokemon("SandSlash", "ground", 100, 110, 75);
-    insertMove("Growl", "normal", 0, 100);
-    insertMove("Tackle", "normal", 40, 100);
-    insertMove("Vine Whip", "grass", 45, 100);
-    insertMove("Leech Seed", "grass", 0, 90);
-    insertMove("Power Whip", "grass", 120, 85);
-    insertMove("Scratch", "normal", 40, 100);
-    insertMove("Smokescreen", "normal", 0, 100);
-    insertMove("Ember", "fire", 40, 100);
-    insertMove("Heat Wave", "fire", 95, 90);
-    insertMove("Tail Whip", "normal", 0, 100);
-    insertMove("Bubble", "water", 40, 100);
-    insertMove("Water Gun", "water", 40, 100);
-    insertMove("Aqua Jet", "water", 40, 100);
-    insertMove("Hydro Pump", "water", 110, 80);
-    insertMove("String shot", "bug", 0, 95);
-    insertMove("Harden", "normal", 0, 100);
-    insertMove("Gust", "flying", 50, 100);
-    insertMove("Sleep Powder", "grass", 0, 100);
-    insertMove("Psybeam", "psychic", 65, 100);
-    insertMove("Poison Sting", "poison", 15, 100);
-    insertMove("Peck", "flying", 35, 100);
-    insertMove("Twineedle", "grass", 25, 100);
-    insertMove("Rage", "normal", 15, 85);
-    insertMove("Poison Jab", "poison", 80, 100);
-    insertMove("Quick Attack", "normal", 40, 100);
-    insertMove("Sky Attack", "flying", 140, 90);
-    insertMove("Bite", "dark", 60, 100);
-    insertMove("Fury Attack", "normal", 15, 85);
-    insertMove("Drill Peck", "normal", 80, 100);
-    insertMove("Wrap", "normal", 15, 90);
-    insertMove("Acid", "poison", 40, 100);
-    insertMove("Sucker Punch", "dark", 70, 100);
-    insertMove("Dig", "ground", 80, 100);
-    insertMove("Fury Swipes", "normal", 18, 80);
-    insertMove("Defense Curl", "normal", 0, 100);
-    insertMove("Slash", "ground", 70, 100);
-    insertMove("Thunder Shock", "electric", 40, 100);
-    insertMove("Thunder Punch", "electric", 75, 100);
-
-    // bulbasaur
-    insertMovePokemon(1, 1);
-    insertMovePokemon(1, 2);
-    insertMovePokemon(1, 3);
-    insertMovePokemon(1, 4);
-
-    // ivysaur
-    insertMovePokemon(2, 1);
-    insertMovePokemon(2, 2);
-    insertMovePokemon(2, 3);
-    insertMovePokemon(2, 4);
-
-    // // venasaur
-    insertMovePokemon(3, 1);
-    insertMovePokemon(3, 3);
-    insertMovePokemon(3, 4);
-    insertMovePokemon(3, 5);
-
-    // // charmander
-    insertMovePokemon(4, 6);
-    insertMovePokemon(4, 1);
-    insertMovePokemon(4, 7);
-    insertMovePokemon(4, 8);
-
-    // charmeleon
-    insertMovePokemon(5, 6);
-    insertMovePokemon(5, 1);
-    insertMovePokemon(5, 7);
-    insertMovePokemon(5, 8);
-
-    // charizard
-    insertMovePokemon(6, 1);
-    insertMovePokemon(6, 6);
-    insertMovePokemon(6, 7);
-    insertMovePokemon(6, 8);
-
-    // squirtle
-    insertMovePokemon(7, 10);
-    insertMovePokemon(7, 11);
-    insertMovePokemon(7, 12);
-    insertMovePokemon(7, 18);
-
-    // wartortle
-    insertMovePokemon(8, 10);
-    insertMovePokemon(8, 11);
-    insertMovePokemon(8, 12);
-    insertMovePokemon(8, 18);
-
-    // // blastoise
-    // insertMovePokemon(9, 12);
-    // insertMovePokemon(9, 13);
-    // insertMovePokemon(9, 14);
-    // insertMovePokemon(9, 15);
-
-    // caterpie
-    insertMovePokemon(10, 10);
-    insertMovePokemon(10, 16);
-
-    // metapod
-    insertMovePokemon(11, 17);
-
-    // butterfree
-    insertMovePokemon(12, 18);
-    insertMovePokemon(12, 19);
-    insertMovePokemon(12, 20);
-
-    // weedle
-    insertMovePokemon(13, 21);
-    insertMovePokemon(13, 16);
-
-    // kakuna
-    insertMovePokemon(14, 17);
-
-    // // beedrill
-    insertMovePokemon(15, 22);
-    insertMovePokemon(15, 23);
-    insertMovePokemon(15, 24);
-    insertMovePokemon(15, 29);
-
-    // pidgey
-    insertMovePokemon(16, 2);
-    insertMovePokemon(16, 22);
-    insertMovePokemon(16, 11);
-    insertMovePokemon(16, 18);
-
-    // pidget
-    insertMovePokemon(17, 2);
-    insertMovePokemon(17, 22);
-    insertMovePokemon(17, 11);
-    insertMovePokemon(17, 18);
-
-    // pidgeotto
-    insertMovePokemon(18, 22);
-    insertMovePokemon(18, 11);
-    insertMovePokemon(18, 18);
-    insertMovePokemon(18, 26);
-
-    // rattata
-    insertMovePokemon(19, 10);
-    insertMovePokemon(19, 11);
-    insertMovePokemon(19, 26);
-    insertMovePokemon(19, 33);
-
-    // raticate
-    insertMovePokemon(20, 10);
-    insertMovePokemon(20, 35);
-    insertMovePokemon(20, 26);
-
-    // spearow
-    insertMovePokemon(21, 22);
-    insertMovePokemon(21, 29);
-    insertMovePokemon(21, 30);
-
-    // fearow
-    insertMovePokemon(22, 22);
-    insertMovePokemon(22, 26);
-    insertMovePokemon(22, 27);
-    insertMovePokemon(22, 30);
-
-    // ekans
-    insertMovePokemon(23, 32);
-    insertMovePokemon(23, 21);
-    insertMovePokemon(23, 31);
-
-    // arbok
-    insertMovePokemon(24, 32);
-    insertMovePokemon(24, 21);
-    insertMovePokemon(24, 31);
-    insertMovePokemon(24, 25);
-
-    // pikachu
-    insertMovePokemon(25, 1);
-    insertMovePokemon(25, 26);
-    insertMovePokemon(25, 38);
-
-    // raichu
-    insertMovePokemon(26, 1);
-    insertMovePokemon(26, 26);
-    insertMovePokemon(26, 38);
-    insertMovePokemon(26, 39);
-
-    // sandshrew
-    insertMovePokemon(27, 36);
-    insertMovePokemon(27, 6);
-    insertMovePokemon(27, 37);
-
-    // sandslash
-    insertMovePokemon(28, 36);
-    insertMovePokemon(28, 6);
-    insertMovePokemon(28, 37);
-    insertMovePokemon(28, 34);
+    // createNewTable(PlayerTable);
+    // createNewTable(PlayerData);
+    // createNewTable(PokedexTable); // all pokemons will be stored here
+    // createNewTable(MovesTable); // all moves will be stored here
+    // createNewTable(PokemonMovesTable); // pokemons with their move will be stored here
+    // createNewTable(PlayerPokemonTable); // player's pokemon will be stored here
+    // // name, type, atk, def, hp
+    // insertPokemon("Bulbasaur", "grass", 49, 49, 45);
+    // insertPokemon("Ivysaur", "grass", 62, 63, 60);
+    // insertPokemon("Venusaur", "grass", 82, 83, 80);
+    // insertPokemon("Charmander", "fire", 52, 43, 39);
+    // insertPokemon("Charmeleon", "fire", 64, 58, 58);
+    // insertPokemon("Charizard", "fire", 84, 78, 78);
+    // insertPokemon("Squirtle", "water", 48, 65, 44);
+    // insertPokemon("Wartortle", "water", 63, 80, 59);
+    // insertPokemon("Blastoise", "water", 83, 100, 79);
+    // insertPokemon("Caterpie", "bug", 30, 35, 45);
+    // insertPokemon("Metapod", "bug", 20, 55, 50);
+    // insertPokemon("Butterfree", "bug", 45, 50, 60);
+    // insertPokemon("Weedle", "bug", 35, 30, 40);
+    // insertPokemon("Kakuna", "bug", 25, 50, 45);
+    // insertPokemon("Beedrill", "bug", 90, 40, 65);
+    // insertPokemon("Pidgey", "flying", 45, 40, 40);
+    // insertPokemon("Pidgeotto", "flying", 60, 55, 63);
+    // insertPokemon("Pidgeot", "flying", 80, 75, 83);
+    // insertPokemon("Rattata", "normal", 56, 35, 30);
+    // insertPokemon("Raticate", "normal", 81, 60, 50);
+    // insertPokemon("Spearow", "flying", 60, 30, 40);
+    // insertPokemon("Fearow", "flying", 90, 65, 65);
+    // insertPokemon("Ekans", "poison", 60, 44, 35);
+    // insertPokemon("Arbok", "poison", 85, 69, 60);
+    // insertPokemon("Pikachu", "electric", 55, 40, 35);
+    // insertPokemon("Raichu", "electric", 90, 55, 60);
+    // insertPokemon("Sandshrew", "ground", 75, 85, 50);
+    // insertPokemon("SandSlash", "ground", 100, 110, 75);
+    // insertMove("Growl", "normal", 0, 100);
+    // insertMove("Tackle", "normal", 40, 100);
+    // insertMove("Vine Whip", "grass", 45, 100);
+    // insertMove("Leech Seed", "grass", 0, 90);
+    // insertMove("Power Whip", "grass", 120, 85);
+    // insertMove("Scratch", "normal", 40, 100);
+    // insertMove("Smokescreen", "normal", 0, 100);
+    // insertMove("Ember", "fire", 40, 100);
+    // insertMove("Heat Wave", "fire", 95, 90);
+    // insertMove("Tail Whip", "normal", 0, 100);
+    // insertMove("Bubble", "water", 40, 100);
+    // insertMove("Water Gun", "water", 40, 100);
+    // insertMove("Aqua Jet", "water", 40, 100);
+    // insertMove("Hydro Pump", "water", 110, 80);
+    // insertMove("String shot", "bug", 0, 95);
+    // insertMove("Harden", "normal", 0, 100);
+    // insertMove("Gust", "flying", 50, 100);
+    // insertMove("Sleep Powder", "grass", 0, 100);
+    // insertMove("Psybeam", "psychic", 65, 100);
+    // insertMove("Poison Sting", "poison", 15, 100);
+    // insertMove("Peck", "flying", 35, 100);
+    // insertMove("Twineedle", "grass", 25, 100);
+    // insertMove("Rage", "normal", 15, 85);
+    // insertMove("Poison Jab", "poison", 80, 100);
+    // insertMove("Quick Attack", "normal", 40, 100);
+    // insertMove("Sky Attack", "flying", 140, 90);
+    // insertMove("Bite", "dark", 60, 100);
+    // insertMove("Fury Attack", "normal", 15, 85);
+    // insertMove("Drill Peck", "normal", 80, 100);
+    // insertMove("Wrap", "normal", 15, 90);
+    // insertMove("Acid", "poison", 40, 100);
+    // insertMove("Sucker Punch", "dark", 70, 100);
+    // insertMove("Dig", "ground", 80, 100);
+    // insertMove("Fury Swipes", "normal", 18, 80);
+    // insertMove("Defense Curl", "normal", 0, 100);
+    // insertMove("Slash", "ground", 70, 100);
+    // insertMove("Thunder Shock", "electric", 40, 100);
+    // insertMove("Thunder Punch", "electric", 75, 100);
+    //
+    // // bulbasaur
+    // insertMovePokemon(1, 1);
+    // insertMovePokemon(1, 2);
+    // insertMovePokemon(1, 3);
+    // insertMovePokemon(1, 4);
+    //
+    // // ivysaur
+    // insertMovePokemon(2, 1);
+    // insertMovePokemon(2, 2);
+    // insertMovePokemon(2, 3);
+    // insertMovePokemon(2, 4);
+    //
+    // // // venasaur
+    // insertMovePokemon(3, 1);
+    // insertMovePokemon(3, 3);
+    // insertMovePokemon(3, 4);
+    // insertMovePokemon(3, 5);
+    //
+    // // // charmander
+    // insertMovePokemon(4, 6);
+    // insertMovePokemon(4, 1);
+    // insertMovePokemon(4, 7);
+    // insertMovePokemon(4, 8);
+    //
+    // // charmeleon
+    // insertMovePokemon(5, 6);
+    // insertMovePokemon(5, 1);
+    // insertMovePokemon(5, 7);
+    // insertMovePokemon(5, 8);
+    //
+    // // charizard
+    // insertMovePokemon(6, 1);
+    // insertMovePokemon(6, 6);
+    // insertMovePokemon(6, 7);
+    // insertMovePokemon(6, 8);
+    //
+    // // squirtle
+    // insertMovePokemon(7, 10);
+    // insertMovePokemon(7, 11);
+    // insertMovePokemon(7, 12);
+    // insertMovePokemon(7, 18);
+    //
+    // // wartortle
+    // insertMovePokemon(8, 10);
+    // insertMovePokemon(8, 11);
+    // insertMovePokemon(8, 12);
+    // insertMovePokemon(8, 18);
+    //
+    // // // blastoise
+    // // insertMovePokemon(9, 12);
+    // // insertMovePokemon(9, 13);
+    // // insertMovePokemon(9, 14);
+    // // insertMovePokemon(9, 15);
+    //
+    // // caterpie
+    // insertMovePokemon(10, 10);
+    // insertMovePokemon(10, 16);
+    //
+    // // metapod
+    // insertMovePokemon(11, 17);
+    //
+    // // butterfree
+    // insertMovePokemon(12, 18);
+    // insertMovePokemon(12, 19);
+    // insertMovePokemon(12, 20);
+    //
+    // // weedle
+    // insertMovePokemon(13, 21);
+    // insertMovePokemon(13, 16);
+    //
+    // // kakuna
+    // insertMovePokemon(14, 17);
+    //
+    // // // beedrill
+    // insertMovePokemon(15, 22);
+    // insertMovePokemon(15, 23);
+    // insertMovePokemon(15, 24);
+    // insertMovePokemon(15, 29);
+    //
+    // // pidgey
+    // insertMovePokemon(16, 2);
+    // insertMovePokemon(16, 22);
+    // insertMovePokemon(16, 11);
+    // insertMovePokemon(16, 18);
+    //
+    // // pidget
+    // insertMovePokemon(17, 2);
+    // insertMovePokemon(17, 22);
+    // insertMovePokemon(17, 11);
+    // insertMovePokemon(17, 18);
+    //
+    // // pidgeotto
+    // insertMovePokemon(18, 22);
+    // insertMovePokemon(18, 11);
+    // insertMovePokemon(18, 18);
+    // insertMovePokemon(18, 26);
+    //
+    // // rattata
+    // insertMovePokemon(19, 10);
+    // insertMovePokemon(19, 11);
+    // insertMovePokemon(19, 26);
+    // insertMovePokemon(19, 33);
+    //
+    // // raticate
+    // insertMovePokemon(20, 10);
+    // insertMovePokemon(20, 35);
+    // insertMovePokemon(20, 26);
+    //
+    // // spearow
+    // insertMovePokemon(21, 22);
+    // insertMovePokemon(21, 29);
+    // insertMovePokemon(21, 30);
+    //
+    // // fearow
+    // insertMovePokemon(22, 22);
+    // insertMovePokemon(22, 26);
+    // insertMovePokemon(22, 27);
+    // insertMovePokemon(22, 30);
+    //
+    // // ekans
+    // insertMovePokemon(23, 32);
+    // insertMovePokemon(23, 21);
+    // insertMovePokemon(23, 31);
+    //
+    // // arbok
+    // insertMovePokemon(24, 32);
+    // insertMovePokemon(24, 21);
+    // insertMovePokemon(24, 31);
+    // insertMovePokemon(24, 25);
+    //
+    // // pikachu
+    // insertMovePokemon(25, 1);
+    // insertMovePokemon(25, 26);
+    // insertMovePokemon(25, 38);
+    //
+    // // raichu
+    // insertMovePokemon(26, 1);
+    // insertMovePokemon(26, 26);
+    // insertMovePokemon(26, 38);
+    // insertMovePokemon(26, 39);
+    //
+    // // sandshrew
+    // insertMovePokemon(27, 36);
+    // insertMovePokemon(27, 6);
+    // insertMovePokemon(27, 37);
+    //
+    // // sandslash
+    // insertMovePokemon(28, 36);
+    // insertMovePokemon(28, 6);
+    // insertMovePokemon(28, 37);
+    // insertMovePokemon(28, 34);
+    // createUser("admin", "helloworld123", "adminator");
 
   }
 
@@ -410,6 +411,8 @@ public class Database {
       System.exit(0);
     }
   }
+
+
 
   /**
    * This will create a new table on the database Pokemon_db
